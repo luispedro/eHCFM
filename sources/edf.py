df1078e95c884d6645a9ce0959eb0d27d5fb82ee
Luis Pedro Coelho <luis@luispedro.org>
2017-04-12 11:47:58 +0200

    ENH Chose "most in focus" slice

--- sources/edf.py
+++ sources/edf.py
@@ -1,6 +1,19 @@
 import numpy as np
 import mahotas as mh
+def sobel_edf(image):
+    '''
+    edf = sobel_edf(image)
+    '''
+    stack,h,w = image.shape
+    best = np.argmax([mh.sobel(t, just_filter=True) for t in image], 0)
+    image = image.reshape((stack,-1)).transpose()
+    return image[np.arange(len(image)),best.ravel()].reshape((h,w))
 
+def sobel_best(image):
+    stack,h,w = image.shape
+    best = np.array([mh.sobel(t, just_filter=True).mean() for t in image])
+    best = best.argmax()
+    return image[best]
 
 def cv(x):
     '''
